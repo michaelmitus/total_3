dd210389ad6febe117da099b4a9b34dd0791eca7
michaelmitus <49987038+michaelmitus@users.noreply.github.com>
2019-06-09 23:52:59 +0500

    Add files via upload

---
+++ link_server.py
@@ -0,0 +1,103 @@
+import requests
+import sqlite3 as lite
+import json
+from newsapi import NewsApiClient
+from flask import Flask, request, Response, render_template, jsonify
+
+app = Flask(__name__)
+
+def sql_command(sql_req):
+    con = lite.connect('links.sqlite')
+    curID = con.cursor()
+    curID.executescript(sql_req)
+    resp = curID.fetchall()
+    con.close()
+    return(resp)
+
+def sql_command_lite(sql_req):
+    con = lite.connect('links.sqlite')
+    curID = con.cursor()
+    curID.execute(sql_req)
+    resp = curID.fetchone()
+    con.close()
+    return resp
+
+def short_link_exists(short_link):
+    sql_request = 'SELECT ID FROM Links WHERE Short_link LIKE "%s"' % short_link
+    resp = sql_command_lite(sql_request)
+    return resp
+
+def add_link(user_id, full_link, short_link, access_type):
+#    Добавление ссылки
+    if not short_link_exists(short_link):
+        sql_request = "INSERT INTO Links (Full_Link, Short_Link, UserID, Access) VALUES ('%s','%s', '%s','%s')" % (full_link, short_link, user_id, access_type)
+        sql_command(sql_request)
+        return 'Ok'+sql_request
+    else:
+        return 'Ошибка. Такая короткая ссылка уже зарегистрирована'
+
+def get_links(user_id):
+    sql_request = "SELECT Full_Link, Short_Link, Access FROM Links WHERE UserID='%s'" % (str(user_id))
+    return jsonify(sql_command(sql_request))
+
+def delete_link(short_link):
+    sql_request = "DELETE FROM Links WHERE Short_Link='%s'" % (str(short_link))
+    sql_command(sql_request)
+    return 'Delete '+sql_request
+
+def update_link(full_link, short_link, access_type):
+    sql_request = "DELETE FROM Links WHERE Short_Link='%s'" % (str(short_link))
+    sql_command(sql_request)
+    return 'Delete '+sql_request
+
+def user_exists(name):
+    sql_request = 'SELECT ID FROM Users WHERE Name LIKE "%s"' % name
+    resp = sql_command_lite(sql_request)
+    return resp[0]
+
+def add_user(id_vk, name, password):
+    if not user_exists(name):
+        sql_request = "INSERT INTO Users (vk_ID, name, Password) VALUES ('%s','%s','%s')" % (str(id_vk), name, password)
+        sql_command(sql_request)
+        return "Добавлен пользователь :'%s'" % (name)
+    else:
+        return "Пользователь :'%s' уже зарегистрирован. Выберите другое имя " % (name)
+
+@app.route('/links/', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def links(*args, **kwargs):
+    user_id = request.args.get('user_id')
+    link_id = request.args.get('link_id')
+    full_link = request.args.get('full_link')
+    short_link = request.args.get('short_link')
+    access_type = request.args.get('access_type')
+    if request.method == 'GET':
+        return get_links(user_id)
+    elif request.method == 'POST':
+        return add_link(user_id, full_link, short_link, access_type)
+    elif request.method == 'PATCH':
+        return update_link(full_link, short_link, access_type)
+    elif request.method == 'DELETE':
+        return delete_link(short_link)
+    else:
+        return 'None'
+
+@app.route('/users/', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def users(*args, **kwargs):
+    user_id = request.args.get('user_id')
+    vk_id = request.args.get('vk_id')
+    password = request.args.get('password')
+    name = request.args.get('name')
+    if request.method == 'GET':
+        return get_links(user_id)
+    elif request.method == 'POST':
+        return add_user(vk_id, name, password)
+    elif request.method == 'PATCH':
+        return update_link(full_link, short_link, access_type)
+    elif request.method == 'DELETE':
+        return delete_link(short_link)
+    else:
+        return 'None'
+
+if __name__ == '__main__':
+   app.run (host = '127.0.0.1', port = 8080)
+
---
+++ link_vk.py
@@ -0,0 +1,105 @@
+import requests
+import vk_api
+import bs4
+import json
+from newsapi import NewsApiClient
+from random import randint, choice
+import string
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+vk_token = '9f6ef21d1fa6d22f2cbdff6eaf02dd8a52f05e3e141f0293b6f934006a26ae3d8b4780807cd39efbc07c8'
+vk_session = vk_api.VkApi(token=vk_token)
+
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+
+def write_msg(user_id, message):
+    vk.messages.send(user_id=user_id,
+                     message=message,
+                     random_id=randint(1254353245, 2345378568347568347563453245345345))
+
+def clean_all_tag_from_str(string_line):
+    """
+    Очистка строки stringLine от тэгов и их содержимых
+    :param string_line: Очищаемая строка
+    :return: очищенная строка
+    """
+    result = ""
+    not_skip = True
+    for i in list(string_line):
+        if not_skip:
+            if i == "<":
+                not_skip = False
+            else:
+                result += i
+        else:
+            if i == ">":
+                not_skip = True
+
+    return result
+
+def get_user_name_from_vk_id(user_id):
+    request = requests.get("https://vk.com/id" + str(user_id))
+    bs = bs4.BeautifulSoup(request.text, "html.parser")
+    user_name = clean_all_tag_from_str(bs.findAll("title")[0])
+
+    return user_name.split()[0]
+
+def vk_print(user_id, title, menu_items):
+    msg_text = title + '\n'
+    for items in range(len(menu_items)):
+        msg_text = msg_text + (str(items + 1) + '. ' + str(menu_items[items]) + '\n')
+    write_msg(user_id=user_id,
+              message=msg_text)
+
+def vk_menu(user_id, title, menu_items):
+    vk_print(user_id, title, menu_items)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            if event.from_user or event.from_chat:
+                return event.text
+
+def add_user(user_id):
+    response = requests.post('http://localhost:8080/users/',
+                             params = {'vk_id': user_id, 'name': get_user_name_from_vk_id(user_id)})
+    print(response)
+    return response.text
+
+def add_link(user_id, full_link, short_link, access):
+    response = requests.post('http://localhost:8080/links/',
+                             params = {'user_id': user_id, 'full_link': full_link ,'short_link': short_link, 'access_type': access})
+    print(response)
+    return response.text
+
+def random_string_generator(str_size):
+    return ''.join(choice(string.ascii_letters) for x in range(str_size))
+
+def access_decode(access):
+    if access == 1:
+        return('Публичная')
+    elif access == 2:
+        return ('Общего доступа')
+    else:
+        return ('Приватная')
+
+def main_menu(user_id):
+    vk_print(user_id, 'Главное меню:', ('Регистрация пользователя', 'Ссылки'))
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+   #Слушаем longpoll, если пришло сообщение то:
+        if event.from_user or event.from_chat: #Если написали
+            if event.text == '1':
+                write_msg(event.user_id, add_user(event.user_id))
+            elif event.text == '7':
+                write_msg(event.user_id, random_string_generator(10))
+            elif event.text == '2':
+                full_link = vk_menu(event.user_id, 'Регистрация ссылок:', ('Введите полную ссылку', 'Выход'))
+                short_link = random_string_generator(10)
+                access = int(vk_menu(event.user_id, 'Выберите уровень доступа',
+                                 ('Публичная', 'Общего доступа', 'Приватная')))
+                add_link(event.user_id, full_link, short_link, access)
+                vk_print(event.user_id, 'Cоздана ссылка:',
+                         ('Полая ссыслка: '+full_link, 'Короткая ссылка: '+short_link, 'Уровень доступа: '+access_decode(access)))
+            else:
+                main_menu(event.user_id)
