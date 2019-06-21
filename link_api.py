63c7f2f9f2ee928cba9a6a9cb0e06b43a52fa48a
michaelmitus <49987038+michaelmitus@users.noreply.github.com>
2019-06-22 04:07:50 +0500

    Add files via upload

--- link_api.py
+++ link_api.py
@@ -7,7 +7,35 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.http import HTTP_STATUS_CODES
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 basic_auth = HTTPBasicAuth()
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired
+
+def access_decode(access):
+    if access == 1:
+        return('Публичная')
+    elif access == 2:
+        return ('Общего доступа')
+    else:
+        return ('Приватная')
+
+class LinkForm(FlaskForm):
+    full_link = StringField('Full link', validators=[DataRequired()])
+    short_link = StringField('Short_link', validators=[DataRequired()])
+    access = RadioField('Access', choices=[(1,access_decode(1)),
+                                           (2, access_decode(2)),
+                                           (3, access_decode(3))
+                                            ])
+    submit = SubmitField('Sign In')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 def error_response(status_code, message=None):
     payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
@@ -39,19 +67,49 @@ def basic_auth_error():
 @basic_auth.login_required
 def index2():
     r = requests.get('http://localhost:8080/')
-    return r.text
+    return r
 
 @app.route('/<short_link>')
 @basic_auth.login_required
 def index(short_link):
-    r = requests.get('http://localhost:8080/'+short_link, params = {'user' : g.user, 'password' : g.password})
+    r = requests.get('http://localhost:8080/'+short_link, params={'user': g.user, 'password': g.password})
     return r.text
 
-@app.route('/links/')
+@app.route('/all_links/', methods=['GET', 'POST'])
 @basic_auth.login_required
-def all_links():
+def all_link():
     r = requests.get('http://localhost:8080/links/', headers={'Authorization': "Bearer %s" % g.token})
-    return r.text
+    links = r.json()
+    print(links)
+    return render_template("links.html",
+        title = 'Link list:',
+        name = g.user,
+        links = links)
+
+@app.route('/add_links/', methods=['GET', 'POST'])
+@basic_auth.login_required
+def edit_link():
+    form = LinkForm()
+    print(form.validate_on_submit())
+    if form.full_link.data and form.short_link.data and form.access.data:
+        r = requests.post('http://localhost:8080/links/',
+                         headers={'Authorization': "Bearer %s" % g.token},
+                         params ={
+                             'full_link': form.full_link.data,
+                             'short_link': form.short_link.data,
+                             'access_type': form.access.data
+                                })
+        return redirect('/all_links/')
+    return render_template('edit_link.html', title='Sign In', form=form)
+
+@app.route('/delete_link/<link_id>', methods=['GET', 'POST'])
+@basic_auth.login_required
+def delete_link(link_id):
+    r = requests.delete('http://localhost:8080/links/',
+                     headers={'Authorization': "Bearer %s" % g.token},
+                     params ={
+                         'link_id': link_id})
+    return redirect('/all_links/')
 
 @app.route('/links/<short_link>')
 @basic_auth.login_required
--- link_server.py
+++ link_server.py
@@ -34,6 +34,7 @@ def sql_command_lite(sql_req):
     curID.execute(sql_req)
     resp = curID.fetchone()
     con.close()
+    print(sql_req)
     return resp
 
 def sql_command_get(sql_req):
@@ -166,21 +167,21 @@ def user_ID(name):
     resp = sql_command_lite(sql_request)
     return resp
 
-def add_link(vk_id, full_link, short_link, access_type):
+def add_link(user_id, full_link, short_link, access_type):
 #    Добавление ссылки
     if not short_link_exists(short_link):
-        sql_request = "INSERT INTO Links (Full_Link, Short_Link, UserID, Access) VALUES ('%s','%s', '%s','%s')" % (full_link, short_link, user_ID(vk_id), access_type)
+        sql_request = "INSERT INTO Links (Full_Link, Short_Link, UserID, Access) VALUES ('%s','%s', '%s','%s')" % (full_link, short_link, user_id, access_type)
         sql_command(sql_request)
         return 'Ok'+sql_request
     else:
         return 'Ошибка. Такая короткая ссылка уже зарегистрирована'
 
-def get_links(vk_ID):
-    sql_request = "SELECT Full_Link, Short_Link, Access FROM Links WHERE UserID=%s" % (str(user_ID(vk_ID)))
+def get_links(user_id):
+    sql_request = "SELECT Full_Link, Short_Link, Access FROM Links WHERE UserID=%s" % user_id
     return jsonify(sql_command_get(sql_request))
 
-def delete_link(short_link):
-    sql_request = "DELETE FROM Links WHERE Short_Link='%s'" % (str(short_link))
+def delete_link(id):
+    sql_request = "DELETE FROM Links WHERE ID='%s'" % (str(id))
     sql_command(sql_request)
     return 'Delete '+sql_request
 
@@ -205,22 +206,29 @@ def access_decode(access):
     else:
         return ('Приватная')
 
-def get_links_http(user_id):
-    if user_id > 0 :
-        sql_request = "SELECT Full_Link, Short_Link, Access FROM Links WHERE UserID=%s" % user_id
+def get_links_http(user):
+    user_id = user_ID(user)
+    if user_id:
+        user_id = user_id[0]
     else:
-        sql_request = "SELECT Full_Link, Short_Link, Access FROM Links WHERE Access=%s" % 1
+        user_id = 0
+    print(user_id)
+    if user_id > 0 :
+        sql_request = "SELECT Full_Link, Short_Link, Access, id FROM Links WHERE UserID=%s" % user_id
+    else:
+        sql_request = "SELECT Full_Link, Short_Link, Access, id FROM Links WHERE Access=%s" % 1
     links = sql_command_get(sql_request)
     select = []
 
     for items in range(len(links)):
         select_item = (links[items])
-        select.append({'Full_link': select_item[0], 'Short_link': select_item[1], 'Access': access_decode(select_item[2])})
-
-    return render_template("index.html",
-        title = 'Список ссылок:',
-        user = user_id,
-        links = select)
+        select.append({'Full_link':     select_item[0],
+                       'Short_link':    select_item[1],
+                       'Access_ID':     select_item[2],
+                       'Access':        access_decode(select_item[2]),
+                       'ID':            select_item[3],
+                       })
+    return jsonify(select)
 
 @app.route('/links/', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 @token_auth.login_required
@@ -228,23 +236,26 @@ def links(*args, **kwargs):
     full_link = request.args.get('full_link')
     short_link = request.args.get('short_link')
     access_type = request.args.get('access_type')
+    link_id = request.args.get('link_id')
 
     auth3 = request.headers.get('Authorization').replace('Bearer ','')
     jwt_req = jwt.decode(auth3, 'midis-python', algorithms=['HS256'])
     user_name = jwt_req['name']
 
     user_id = user_ID(user_name)[0]
+
     if not user_id:
         user_id = 0
 
     if request.method == 'GET':
-        return get_links_http(user_id)
+        return get_links_http(user_name)
     elif request.method == 'POST':
+        print(user_id, full_link, short_link, access_type)
         return add_link(user_id, full_link, short_link, access_type)
     elif request.method == 'PATCH':
         return update_link(full_link, short_link, access_type)
     elif request.method == 'DELETE':
-        return delete_link(short_link)
+        return delete_link(link_id)
     else:
         return None
 
@@ -293,7 +304,7 @@ def index(short_link):
                 f_link = full_link(short_link)
                 if f_link:
                     return redirect(f_link[0], code=302)
-    return get_links_http(4)
+    return None
 
 if __name__ == '__main__':
    app.run (host = '127.0.0.1', port = 8080)
